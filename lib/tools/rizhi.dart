a81fa13e7b71bedb73ce763dbf6a18acffbc9fbb
quiz123 <quiz123@desktop>
2025-09-16 03:29:06 -0700

    fix log

--- lib/tools/rizhi.dart
+++ lib/tools/rizhi.dart
@@ -1,9 +1,9 @@
 
 void jcRizhi(String text){
-  print("==pb117: $text");
+  print("==qs123: $text");
 }    
 
 void jcRizhiError(String text){
-  print("==pb117=Error: $text");
+  print("==qs123=Error: $text");
 }
 
